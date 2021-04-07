d67828c89111c41e72182be0866c1906962165bd
DmitryBulychev <81636546+DmitryBulychev@users.noreply.github.com>
2021-04-07 15:06:21 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+           int na, nb;
+           int A[na];//Первый массив
+           int B[nb];//Второй массив
+           int* pa = NULL;
+           int* pb = NULL;
+           printf("na, nb = ");
+           scanf ("%d", &na);//Вводим 100
+           scanf ("%d", &nb);//Вводим 100
+           pa = (int*)calloc(na, sizeof(int));
+           pb = (int*)malloc(nb*sizeof(int));
+           for (int i = 0; i < na; i++) {
+               pa[i] = rand() % 10;
+           }
+           for (int i = 0; i < nb; i++) {
+               pb[i] = rand() % 10;
+           }
+           free(pa);
+           free(pb);
+    return 0;
+}
